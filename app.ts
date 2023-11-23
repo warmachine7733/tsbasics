dd9ac2a7826edb6815038f4ae9b11888f1ec4e9d
prateek jena <prateekjena@prateeks-MacBook-Air.local>
2023-11-23 11:48:48 +0530

    adding interfaces and code splitting

--- app.ts
+++ app.ts
@@ -1,6 +1,12 @@
-const person : { name: string, age: number} = {
+import { Person } from "./interfaces/index"
+
+const person : Person = {
     name: "prateek",
-    age: 29
+    age: 29,
+    phone: {
+        ctryCode: "+91",
+        no: 7008787878
+    }
 }
 const result: string = "the person is:"
 console.log(result, person)
---
+++ interfaces/index.ts
@@ -0,0 +1,8 @@
+export interface Person {
+  name: string,
+  age: number,
+  phone: {
+    ctryCode: string,
+    no: number
+  }
+}
