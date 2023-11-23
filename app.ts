8d40c0b205a780ff68ef4743e8de0b0d1e019571
prateek jena <prateekjena@prateeks-MacBook-Air.local>
2023-11-23 11:35:27 +0530

    types declaration and basics

---
+++ app.ts
@@ -0,0 +1,6 @@
+const person : { name: string, age: number} = {
+    name: "prateek",
+    age: 29
+}
+const result: string = "the person is:"
+console.log(result, person)
---
+++ basics.ts
@@ -0,0 +1,7 @@
+function add(n1: number, n2: number){
+    return n1+n2
+}
+let n1=5
+let n2=2.8
+
+console.log(add(n1,n2))
